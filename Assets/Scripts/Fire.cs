48e4bdff1764757ad37cb9147259ce4ea5058eb1
anilvsajith <anilvsajith@gmail.com>
2016-05-18 16:17:13 +0530

    First commit

---
+++ Assets/Scripts/Fire.cs
@@ -0,0 +1,42 @@
+﻿	using UnityEngine;
+using System.Collections;
+
+public class Fire : MonoBehaviour {
+
+	public int m_PlayerNumber;
+	public Rigidbody BulletPrefab;
+	public float speed = 10f;
+	private string m_Playerfire;
+	private float starttime;
+	private float timepressed;
+	// Use this for initialization
+	void Start () {
+		m_Playerfire = "Fire" + m_PlayerNumber;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetButtonDown(m_Playerfire))
+		{		
+			starttime = Time.time;
+		}
+
+		if(Input.GetButtonUp(m_Playerfire))
+		{
+			timepressed = Time.time - starttime;
+			timepressed = timepressed * 10;
+			timepressed = Mathf.Max (timepressed, 2.0f);
+			timepressed = Mathf.Min (timepressed, 4);
+			//Insatiate a new bullet on this object (which will be our camera)
+			Rigidbody mBullet = Instantiate (BulletPrefab, transform.position, transform.rotation) as Rigidbody;
+
+			//Add force to the bullet to shoot it
+			mBullet.AddForce (transform.forward * 500 * timepressed);
+
+
+			GameObject.Destroy (mBullet.gameObject, 3.0f);
+		}
+
+	}
+}
---
+++ Assets/Scripts/explode.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class explode : MonoBehaviour {
+
+	private GameObject ShellExplosionPrefab; 
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+
+	private bool singlecollision = true;
+	void OnTriggerEnter(Collider collision) {
+		
+			//Find Child and remove its Parent
+			ShellExplosionPrefab = transform.Find ("ShellExplosion").gameObject;
+			ShellExplosionPrefab.transform.parent = null;
+
+			//Play explosion and audio and destroy object
+			Destroy (gameObject);
+			ShellExplosionPrefab.GetComponent<ParticleSystem>().Play();
+			ShellExplosionPrefab.GetComponent<AudioSource> ().Play ();
+			Destroy (ShellExplosionPrefab, 1.0f);
+	}
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+}
